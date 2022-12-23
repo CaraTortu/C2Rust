297a11384c03ba36d5dff5a48e344d5999100754
CaraTortu <javierdj@jadk.com>
2022-12-23 18:42:34 +0000

    0.0.0.0

--- server/main.py
+++ server/main.py
@@ -2,10 +2,10 @@ import socket, threading
 from lib.manage import Connection
 
 s = socket.socket()
-s.bind(("127.0.0.1", 1234))
+s.bind(("0.0.0.0", 1234))
 s.listen(1)
 
-print(f"[i] Listening on 127.0.0.1:1234")
+print(f"[i] Listening on 0.0.0.0:1234")
 
 clients = []
 threads = []
